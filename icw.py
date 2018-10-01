4cdefa39491e93fde32a16c9e09c9d848278bb7e
balakin <balakinvitalyv@gmail.com>
2018-10-01 15:37:55 +0700

    vac system added

--- icw.py
+++ icw.py
@@ -46,24 +46,32 @@ class IcWatcher:
                                  'down_lim': 0, 'err_code': 'I_mes_problem'},
                                 {'func': 'range_state', 'chans': ['Umes'], 'up_lim': 10, 'down_lim': 0,
                                  'err_code': 'U_out_of_range'}]
-        self.conditions_vch1000 = [{'func': 'curr_state', 'chans': ['Iset', 'Imes'], 'wait_time': 3000, 'up_limit:': 5000,
-                                    'down_limit': 0, 'err_code': 'I_mes_problem'},
-                                   {'func': 'is_on', 'chans': ['is_on'], 'err_code': 'PS is off'},
-                                   {'func': 'ilk', 'chans': ['ilk_imax', 'ilk_inverter', 'ilk_out_prot1',
-                                                             'ilk_out_prot2', 'ilk_out_prot3', 'ilk_phase', 'ilk_temp'],
-                                    'wait_time': 3000, 'err_code': 'Interlock'}]
+        self.conditions_vch1000 = [
+            {'func': 'curr_state', 'chans': ['Iset', 'Imes'], 'wait_time': 3000, 'up_limit:': 5000,
+             'down_limit': 0, 'err_code': 'I_mes_problem'},
+            {'func': 'is_on', 'chans': ['is_on'], 'err_code': 'PS is off'},
+            {'func': 'ilk', 'chans': ['ilk_imax', 'ilk_inverter', 'ilk_out_prot1',
+                                      'ilk_out_prot2', 'ilk_out_prot3', 'ilk_phase', 'ilk_temp'],
+             'wait_time': 3000, 'err_code': 'Interlock'}]
+        self.conditions_ist = [
+            {'func': 'curr_state', 'chans': ['Iset', 'Imes'], 'wait_time': 3000, 'up_limit:': 5000,
+             'down_limit': 0, 'err_code': 'I_mes_problem'},
+            {'func': 'is_on', 'chans': ['is_on'], 'err_code': 'PS is off'},
+            {'func': 'ilk', 'chans': ['ilk_imax', 'ilk_umax', 'ilk_out_pro', 'ilk_phase', 'ilk_temp', 'ilk_water',
+                                      'ilk_battery'], 'wait_time': 3000, 'err_code': 'Interlock'}]
 
         self.conditions_dict = {'UM15': self.conditions_um15, 'UM4': self.conditions_um4, 'vaciva': self.conditions_vs,
                                 'vac124': self.conditions_vs, 'vch300': self.conditions_vch300,
                                 'v300': self.conditions_v300, 'pa10': self.conditions_pa10,
-                                'vch1000': self.conditions_vch1000}
+                                'vch1000': self.conditions_vch1000, 'ist': self.conditions_ist}
         self.state_chans_dict = {'magnet': [], 'ion_pump': []}
         self.choose_state_dict = {'UM15': 'magnet', 'UM4': 'magnet', 'vaciva': 'ion_pump', 'vac124': 'ion_pump',
-                                  'vch300': 'magnet', 'v300': 'magnet', 'pa10': 'magnet', 'vch1000': 'magnet'}
+                                  'vch300': 'magnet', 'v300': 'magnet', 'pa10': 'magnet', 'vch1000': 'magnet',
+                                  'ist': 'magnet'}
         self.chans_dict = {'UM15': [], 'UM4': [], 'vaciva': [], 'vac124': [], 'vch300': [], 'v300': [], 'pa10': [],
-                           'vch1000': []}
+                           'vch1000': [], 'ist': []}
         self.devnames_dict = {'UM15': [], 'UM4': [], 'vaciva': [], 'vac124': [], 'vch300': [], 'v300': [], 'pa10': [],
-                              'vch1000': []}
+                              'vch1000': [], 'ist': []}
 
         self.cur = self.conn.cursor()
         self.cur.execute("select devtype.name, chan.name from chan,devtype_chans,devtype "
@@ -75,16 +83,17 @@ class IcWatcher:
 
         self.cur.execute("select devtype.name, chan.name from chan,devtype_chans,devtype "
                          "where chan.id=devtype_chans.chan_id and devtype.id=devtype_chans.devtype_id and "
-                         "devtype.name in ('UM4', 'UM15', 'vaciva', 'vac124', 'vch300', 'v300', 'pa10', 'vch1000') group by grouping sets((devtype.name, chan.name))")
+                         "devtype.name in ('UM4', 'UM15', 'vaciva', 'vac124', 'vch300', 'v300', 'pa10', 'vch1000', 'ist') group by grouping sets((devtype.name, chan.name))")
         for elem in self.cur.fetchall():
             self.chans_dict[elem[0]].append(elem[1])
         print(self.chans_dict)
 
         self.cur.execute("select devtype.name, namesys.name || '.' || dev.name as full_name from dev,dev_devtype,devtype, namesys "
                          "where dev.id=dev_devtype.dev_id and devtype.id=dev_devtype.devtype_id and namesys.id=dev.namesys_id and "
-                         "devtype.name in ('UM4', 'UM15', 'vaciva', 'vac124', 'vch300', 'v300', 'pa10', 'vch1000') group by grouping sets((devtype.name, full_name))")
+                         "devtype.name in ('UM4', 'UM15', 'vaciva', 'vac124', 'vch300', 'v300', 'pa10', 'vch1000', 'ist') group by grouping sets((devtype.name, full_name))")
         for elem in self.cur.fetchall():
             self.devnames_dict[elem[0]].append(elem[1])
+        self.devnames_dict['ist'].append('canhw:11.vit_sim_ist')
         print(self.devnames_dict)
 
         for elem in self.devnames_dict:
@@ -224,7 +233,7 @@ class Cond:
 
         if in_call:
             if self.values[self.dname + '.' + self.dchan]:
-                self.error_code = self.dchan + '|' + self.cnd['err_code'] + '|' + 'auto_is_powerless'
+                self.error_code = self.dchan + '|' + self.cnd['err_code'] + '|' + 'auto_is_usefull'
                 self.error_data_send()
             else:
                 self.error_code = self.dchan + '|' + self.cnd['err_code'] + '|' + 'auto_is_turned_on'
